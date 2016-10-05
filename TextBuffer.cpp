ea87749bc0c56a99282a8f51a15052289d253e5b
stagf15 <phil4200@hotmail.com>
2016-10-05 05:51:01 -0500

    Add files via upload

---
+++ TextBuffer.cpp
@@ -0,0 +1,214 @@
+﻿#include "Arduino.h"
+#include "TextBuffer.h"
+
+TextBuffer::TextBuffer(unsigned int bufSize)
+  {
+    _bufSize = (bufSize + 3) & (~3);  // Makes sure it's a multiple of 4
+                                      //   - required for the ESP8266 boards
+    //begin();                        // Automatically call the begin fuction
+                                      //   - not used
+  }
+
+int TextBuffer::begin()
+  {
+    buffer = (byte*)malloc(sizeof(byte)*_bufSize);
+    
+    if (!buffer) return 0;            // return failure if malloc fails
+    
+    buffer[0] = (char)0;              // Initialize first byte as null terminator
+    capacity = _bufSize;
+    position = 0;
+    length = 0;                       // Length does not include null terminator
+    return 1;                         // return success
+  }
+
+int TextBuffer::write(char* bufData)
+  {
+    String bufString = String(bufData);
+    return bufWrite(bufString);
+  }
+
+int TextBuffer::writeln(char* bufData)
+  {
+    String bufString = String(bufData)  + "\r\n";
+    return bufWrite(bufString);
+  }
+
+int TextBuffer::write(char bufData)
+  {
+    String bufString = String(bufData);
+    return bufWrite(bufString);
+  }
+
+int TextBuffer::writeln(char bufData)
+  {
+    String bufString = String(bufData)  + "\r\n";
+    return bufWrite(bufString);
+  }
+
+int TextBuffer::write(int bufData)
+  {
+    String bufString = String(bufData);
+    return bufWrite(bufString);
+  }
+
+int TextBuffer::writeln(int bufData)
+  {
+    String bufString = String(bufData)  + "\r\n";
+    return bufWrite(bufString);
+  }
+
+int TextBuffer::write(long unsigned int bufData)
+  {
+    String bufString = String(bufData);
+    return bufWrite(bufString);
+  }
+
+int TextBuffer::writeln(long unsigned int bufData)
+  {
+    String bufString = String(bufData)  + "\r\n";
+    return bufWrite(bufString);
+  }
+
+int TextBuffer::write(size_t bufData)
+  {
+    String bufString = String(bufData);
+    return bufWrite(bufString);
+  }
+
+int TextBuffer::writeln(size_t bufData)
+  {
+    String bufString = String(bufData)  + "\r\n";
+    return bufWrite(bufString);
+  }
+
+int TextBuffer::write(float bufData)
+  {
+    String bufString = String(bufData);
+    return bufWrite(bufString);
+  }
+
+int TextBuffer::writeln(float bufData)
+  {
+    String bufString = String(bufData)  + "\r\n";
+    return bufWrite(bufString);
+  }
+
+int TextBuffer::write(double bufData)
+  {
+    String bufString = String(bufData);
+    return bufWrite(bufString);
+  }
+
+int TextBuffer::writeln(double bufData)
+  {
+    String bufString = String(bufData)  + "\r\n";
+    return bufWrite(bufString);
+  }
+
+int TextBuffer::write(String bufData)
+  {
+    String bufString = String(bufData);
+    return bufWrite(bufString);
+  }
+
+int TextBuffer::writeln(String bufData)
+  {
+    String bufString = String(bufData)  + "\r\n";
+    return bufWrite(bufString);
+  }
+
+int TextBuffer::write()
+  {
+    return bufWrite("");
+  }
+
+int TextBuffer::writeln()
+  {
+    return bufWrite("\r\n");
+  }
+
+int TextBuffer::bufWrite(String bufString)
+  {
+    unsigned int dataLen = bufString.length() + 1;
+    char tempBuf[dataLen];
+    
+    bufString.toCharArray(tempBuf, dataLen);
+    
+    if((length + dataLen) < capacity)
+      {
+        for (int bt = 0; bt < (dataLen); bt++)
+          {
+            // save each data byte to the end of the buffer
+            buffer[((position+length) % capacity)] = tempBuf[bt];
+            // increment the length unless it's the last byte (null terminator)
+            if (bt != (dataLen - 1)) length++;
+          }
+        return 1;                 // return success
+      }
+    return 0;                     // return failure
+  }
+  
+int TextBuffer::clear() 
+  {
+    if (!buffer) return 0;        // return failure
+    buffer[0] = (char)0;
+    position = 0;
+    length = 0;
+    return 1;                     // return success
+  } 
+
+int TextBuffer::end()
+  {
+    if (!buffer) return 0;        // return failure
+    free(buffer);
+    return 1;                     // return success
+  }
+
+const char* TextBuffer::getBuffer() 
+  {
+    if (!buffer) return (char)0;  // return null terminator
+    return (const char*)buffer;   // return const char array buffer pointer
+  }
+  
+char* TextBuffer::getBufPointer() 
+  {
+    if (!buffer) return 0;        // return failure
+    clear();                      // clear the buffer
+    return (char*)buffer;         // return char array buffer pointer
+  }
+  
+int TextBuffer::getSize()
+  {
+    if (!buffer) return 0;        // return failure
+      if (strlen((const char*)buffer) != length)
+        // if the length is not correct, probably due to an external
+        // write to the buffer, reset it to the correct length
+        {
+          length = strlen((const char*)buffer);
+        }
+    return length;
+  }
+
+int TextBuffer::getCapacity()
+  {
+    if (!buffer) return 0;        // return failure
+    return capacity;
+  }
+  
+String TextBuffer::getCheckSum()
+  {
+    // Create Checksum
+    char checkSum = 0;
+    int csCount = 1;
+    while (buffer[csCount + 1] != 0)
+    {
+      checkSum ^= buffer[csCount];
+      csCount++;
+    }
+    // Change the checksum to a string, in HEX form, convert to upper case, and print
+    String checkSumStr = String(checkSum, HEX);
+    checkSumStr.toUpperCase();
+    
+    return checkSumStr;
+  }
---
+++ TextBuffer.h
@@ -0,0 +1,124 @@
+﻿/*
+
+  TextBuffer.h - Library for writing various types to a buffer
+  that will store them in a char array of predetermined length
+  
+  Created by stagf15, 19 Jul 2016.
+  Released into the public domain.
+  
+*/
+
+#ifndef TextBuffer_h
+#define TextBuffer_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+
+class TextBuffer
+{
+    
+  public:
+    
+    // In general, functions will return the following:
+    //    If failure, return int 0, or null terminator: (char)0
+    //    If success, return int 1, or desired data
+    
+    // If malloc fails in begin() then it will return 0
+    //   - all other functions will then return failure
+    //   - if this is would cause failure in the sketch, recommend
+    //       checking begin() for failure and handling there
+    
+    // Class to call to initialize the buffer (before Setup). Requires the 
+    // desired size of the buffer in bytes. The size is then checked and 
+    // converted, if required, to a multiple of 4. (for ESP8266 boards)
+    TextBuffer(unsigned int bufSize);
+    
+    // Begins the buffer; allocates the memory (in Setup)
+    int begin();
+    
+    // The following functions accept various types as input, but all write a 
+    // string representation of that type to the buffer  
+    
+    // For each type, there is a write(bufData), which will write just the 
+    // string, and a writeln(bufData), which will add a '\r\n' to the string 
+    // to signify the end of a line
+    
+    int write(char* bufData);
+    int writeln(char* bufData);
+    int write(char bufData);
+    int writeln(char bufData);
+    int write(int bufData);
+    int writeln(int bufData);
+    int write(long unsigned int bufData);
+    int writeln(long unsigned int bufData);
+    int write(size_t bufData);
+    int writeln(size_t bufData);
+    int write(float bufData);
+    int writeln(float bufData);
+    int write(double bufData);
+    int writeln(double bufData);
+    int write(String bufData);
+    int writeln(String bufData);
+    int write();
+    int writeln();
+    int bufWrite(String bufData);
+    String getCheckSum();
+    
+    // Clears the buffer, writes a null terminator to byte 0 and 
+    // resets length and position to 0
+    int clear();
+    
+    // Ends, or de-allocates the buffer and frees the memory   
+    // Requires a begin() to use the buffer again
+    int end();
+    
+    // Returns a pointer to the buffer as a const char array
+    //   Used to return the buffer itself
+    const char* getBuffer();
+    
+    // Returns a pointer to the buffer as a char array
+    //   Used to modify the buffer directly from the calling sketch
+    //   - this could be dangerous, if the capacity is not checked prior!
+    //   - calling this will overwrite anything in the buffer
+    char* getBufPointer();
+    
+    // Returns the size (length) of the buffer, not including null terminator
+    int getSize();
+    
+    // Returns the capacity of the buffer, equal to the bufsize passed initially
+    int getCapacity();
+    
+  private:  
+  
+    // Pointer to the buffer char array
+    byte* buffer;
+    
+    // Variable to hold the aligned buffer max capacity
+    //   - Aligned to multiples of 4 bytes for the ESP8266 boards
+    unsigned int _bufSize;
+    
+    // Same as _bufSize, the max capacity, including null terminator, of the buffer
+    unsigned int capacity;
+    
+    // The position of the "cursor", or reference point in the buffer (usually 0)
+    unsigned int position;
+    
+    // Current length of the buffer content, NOT including the null terminator
+    unsigned int length;
+    
+    // String used to temporarily store each individual input to the buffer
+    String bufString;
+    
+    // The length of bufString, plus one for the null terminator
+    unsigned int dataLen;
+    
+    // A char array, of length dataLen, to hold the bufString
+    char tempBuf[];
+
+};
+
+#endif
